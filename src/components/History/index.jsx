3be495c8a8b9bf9332e6bddcbd2550a359132474
Antonio-Alisson-Teixeira-Cruz <ravenjouki@gmail.com>
2025-06-16 09:42:52 -0300

    Update index.jsx

--- src/components/History/index.jsx
+++ src/components/History/index.jsx
@@ -1,7 +1,9 @@
+import './style.css'
+
 export default function History({ arr }) {
     return (
         <div>
-            <h2>Histórico:</h2>
+            <h2 className="history">Histórico:</h2>
             {arr.map((item, index) => (
                 <div key={index}>
                     <p>{item}</p>
